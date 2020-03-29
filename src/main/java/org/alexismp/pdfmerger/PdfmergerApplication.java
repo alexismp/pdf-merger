e97424994ca684282043e8a44fff2838e7c87037
alexismp <alexismp@google.com>
2020-03-29 15:09:22 +0200

    init project

---
+++ src/main/java/org/alexismp/pdfmerger/PdfmergerApplication.java
@@ -0,0 +1,13 @@
+package org.alexismp.pdfmerger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfmergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfmergerApplication.class, args);
+	}
+
+}
